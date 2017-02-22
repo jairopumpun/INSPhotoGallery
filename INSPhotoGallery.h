d2ff5bb221de04a5ff3bb0af517642202fe07747
Jairo Chapela-Martínez <jairo@pumpun.com>
2017-02-22 17:01:17 +0100

    lost files

---
+++ INSPhotoGallery.h
@@ -0,0 +1,19 @@
+//
+//  INSPhotoGallery.h
+//  INSPhotoGallery
+//
+//  Created by Jairo Chapela on 22/02/17.
+//  Copyright © 2017 pumpun4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for INSPhotoGallery.
+FOUNDATION_EXPORT double INSPhotoGalleryVersionNumber;
+
+//! Project version string for INSPhotoGallery.
+FOUNDATION_EXPORT const unsigned char INSPhotoGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INSPhotoGallery/PublicHeader.h>
+
+
